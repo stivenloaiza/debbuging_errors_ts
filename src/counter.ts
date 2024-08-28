11267aef59b9007e5417e0d769c6ae81b52045aa
unknown <loaizadeveloper@gmail.com>
2024-08-28 11:43:05 -0500

    solved fix

--- src/counter.ts
+++ src/counter.ts
@@ -3,21 +3,33 @@ export function setupCounter(element: HTMLButtonElement) {
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
+    if(counter >=1){
+      execErrors();
+    }
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
 
+}
 
+function execErrors(){
   const resultAdd = addNumbers(10, 0);
   console.log(resultAdd);
+  const resultEven = isEven(10);
+  console.log(resultEven);
   const resultDivide = divideNumbers(10, 0); // Provoca un error en tiempo de ejecución (división por cero)
   console.log(resultDivide);
-  printUserName(null);
+  console.log(typeof resultDivide);
+  printUserName({name: "Mil", age: 33});
+
+  const myArray = [1, 2, 3];
+  const indexItem = getItemFromArray(myArray, 3);
+  console.log(indexItem);
 }
 
 
 // Errores de Sintaxis
-function addNumbers(a: number, b: number { // Falta un paréntesis de cierre
+function addNumbers(a: number, b: number) { // Falta un paréntesis de cierre
   return a + b;
 }
 
@@ -37,6 +49,14 @@ interface User {
   name: string;
   age: number;
 }
-function printUserName(user: User | null) {
+function printUserName(user: User) {
   console.log(user.name); // Error de referencia: posible acceso a propiedad de un objeto null
+}
+
+// Errores en Tiempo de Ejecución (Runtime Errors)
+function getItemFromArray(arr: number[], index: number): string {
+  if(index >= arr.length){
+    return 'The index is out of range';
+  }
+  return arr[index + 1].toFixed(2); // Intentar acceder a una propiedad de un número generará un error de ejecución
 }
